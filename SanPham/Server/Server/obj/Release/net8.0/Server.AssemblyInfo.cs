e9b9420126bd47bc8af9ccd7a012fb2c58dc6d5f
Ngoan <ngoanlesqtt@gmail.com>
2024-12-21 02:18:42 +0700

    Cập nhập Em

--- SanPham/Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ SanPham/Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d213832506278973a4d374f61550f255430b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
